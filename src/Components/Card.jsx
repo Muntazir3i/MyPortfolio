04cb1bb61f4e06920e40326cc1647175098488fb
mohammad <mohammadmunti.5@gmail.com>
2025-04-12 09:35:57 +0530

    text color big fixed in project section

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,7 +8,7 @@ function Card({title,desp,link}) {
     <a href={link} id='card-link' className={`block p-6 mb-3   rounded-lg shadow ${dark ? 'bg-diagonal-lines' : 'bg-white'}`}>
 
 <h5 className={`mb-2 text-2xl font-bold tracking-tight ${dark ? 'text-white' : 'text-black'}`}>{title}</h5>
-<p className="font-normal text-gray-700 dark:text-white">{desp}</p>
+<p className={`font-normal ${dark ? 'text-white' : 'text-black'}`}>{desp}</p>
 </a>
   )
 }
